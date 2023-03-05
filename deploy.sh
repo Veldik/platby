9552d047c85ee0ba7bbafcc458f8d9e088130ec9
Jakub Velička <jakub@velicka.eu>
2023-03-05 22:20:18 +0100

    Moc tomu nevěřím, řešení od OpenAI ChatGPT

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,17 @@ APP_NAME=$(grep APP_NAME .env | cut -d '=' -f2)
 echo "Deploying ${APP_NAME}"
 
 # Enter maintenance mode
-(docker exec ${APP_NAME} php artisan down --message 'Platby prochází updatem. Vyzkoušejte to znovu za minutu.') || true
+(docker exec "${APP_NAME}" php artisan down --message 'Platby prochází updatem. Vyzkoušejte to znovu za minutu.') || true
     # Update codebase
     git fetch origin main
     git reset --hard origin/main
 
     # Install dependencies based on lock file
-    docker exec ${APP_NAME} /usr/local/bin/composer install
+    docker exec "${APP_NAME}" /usr/local/bin/composer install
 
     # Migrate database
-    docker exec ${APP_NAME} php artisan migrate --force
+    docker exec "${APP_NAME}" php artisan migrate --force
 # Exit maintenance mode
-docker exec ${APP_NAME} php artisan up
+docker exec "${APP_NAME}" php artisan up
 
-echo "Application ${APP_NAME}!"
+echo "Application ${APP_NAME} deployed!"
