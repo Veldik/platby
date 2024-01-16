1b65387f1a59781d4db1f5ce9fccbce3cbc29493
Jakub Velička <jakub@velicka.eu>
2024-01-16 20:46:32 +0100

    Oprava formátu mailu

--- resources/views/emails/admin-paid-wrong.blade.php
+++ resources/views/emails/admin-paid-wrong.blade.php
@@ -9,7 +9,7 @@
 
         Ahoj! <br/>
 
-        Chci tě informovat, že <b>{{ $payment['name'] }}</b> ti odeslal špatnou částku k platbě <b>{{ $payment['title'] }}</b>. Měl ti poslat <b>{{ $payment['amount'] }} CZK</b>, ale v reálu ti dorazilo <b>{{ $payment['realamount'] }} CZK</b>. Bude super, když se s ním spojíš a pokusíte se společně najít řešení této situace. <br/>
+        Chci tě informovat, že <b>{{ $payment['name'] }}</b> ti odeslal špatnou částku k platbě <b>{{ $payment['title'] }}</b>. Měl ti poslat <b>{{ $payment['amount'] }}</b>, ale v reálu ti dorazilo <b>{{ $payment['realamount'] }}</b>. Bude super, když se s ním spojíš a pokusíte se společně najít řešení této situace. <br/>
 
         Přeji hezký den!
     </div>
